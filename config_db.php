389c6820f9f55a294d0faa6771e17089f29f484e
Paul Okello <paulokello273@gmail.com>
2021-07-20 14:32:57 +0300

    🎉 Initial Commit: Database configuration

---
+++ config_db.php
@@ -0,0 +1,13 @@
+<?php
+// Database host
+$db_host = "localhost";
+// Database name
+$db_name = "db_name";
+// Database user
+$db_user = "db_user";
+// Database password
+$db_pass = "db_pass";
+
+// sql database connection
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+?>
